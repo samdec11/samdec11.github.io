097f10fffa21b766e330d0291745f36c1fce4721
Sean Marzug-McCarthy <samdec11@gmail.com>
2015-08-27 02:29:37 -0400

    intro with logo

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -1,18 +1,57 @@
 $(function() {
-  $(window).scroll(function() {
-  var scr = $(this).scrollTop() / 32
-  var nscr = -($(this).scrollTop() / 16)
-  $('.big-gear').css({
-    '-webkit-transform' : 'rotate('+ scr +'deg)',
-    '-moz-transform' : 'rotate('+ scr +'deg)',
-    '-ms-transform' : 'rotate('+ scr +'deg)',
-    'transform' : 'rotate('+ scr +'deg)'
-    });
+  intro();
+  // $(window).scroll(function() {
+  // var scr = $(this).scrollTop() / 32
+  // var nscr = -($(this).scrollTop() / 16)
+  // $('.big-gear').css({
+  //   '-webkit-transform' : 'rotate('+ scr +'deg)',
+  //   '-moz-transform' : 'rotate('+ scr +'deg)',
+  //   '-ms-transform' : 'rotate('+ scr +'deg)',
+  //   'transform' : 'rotate('+ scr +'deg)'
+  //   });
   // $('.little-gear').css({
   //   '-webkit-transform' : 'rotate('+ nscr +'deg)',
   //   '-moz-transform' : 'rotate('+ nscr +'deg)',
   //   '-ms-transform' : 'rotate('+ nscr +'deg)',
   //   'transform' : 'rotate('+ nscr +'deg)'
   //   })
-  })
+  // })
 });
+
+function intro() {
+  highlight('S');
+  setTimeout(function() {
+    highlight('E');
+    setTimeout(function() {
+      highlight('A');
+      setTimeout(function() {
+        highlight('N');
+        setTimeout(function() {
+          $('.overlay').fadeOut(2000);
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000);
+};
+
+function highlight(letter) {
+  var lines = []
+  $('line').css('stroke', 'gray')
+  switch(letter) {
+    case 'S':
+      lines = [$('.top-line'), $('.diag-line'), $('.btm-line')];
+      break;
+    case 'E':
+      lines = [$('.top-line'), $('.left-line'), $('.mid-line'), $('.btm-line')];
+      break;
+    case 'A':
+      lines = [$('.left-line'), $('.mid-line'), $('.diag-line')];
+      break;
+    case 'N':
+      lines = [$('.left-line'), $('.diag-line'), $('.right-line')];
+      break;
+  }
+  for(i = 0; i < lines.length; i++) {
+    lines[i].css('stroke', 'red')
+  }
+};
