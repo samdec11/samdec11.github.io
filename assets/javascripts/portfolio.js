12cf9a85c8fe5c2c57a0eda5ced5eb6b95975777
Sean Marzug-McCarthy <samdec11@gmail.com>
2015-10-04 23:37:25 -0400

    removed gears, made hovering logo, styled fine print

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -1,21 +1,5 @@
 $(function() {
-  intro();
-  // $(window).scroll(function() {
-  // var scr = $(this).scrollTop() / 32
-  // var nscr = -($(this).scrollTop() / 16)
-  // $('.big-gear').css({
-  //   '-webkit-transform' : 'rotate('+ scr +'deg)',
-  //   '-moz-transform' : 'rotate('+ scr +'deg)',
-  //   '-ms-transform' : 'rotate('+ scr +'deg)',
-  //   'transform' : 'rotate('+ scr +'deg)'
-  //   });
-  // $('.little-gear').css({
-  //   '-webkit-transform' : 'rotate('+ nscr +'deg)',
-  //   '-moz-transform' : 'rotate('+ nscr +'deg)',
-  //   '-ms-transform' : 'rotate('+ nscr +'deg)',
-  //   'transform' : 'rotate('+ nscr +'deg)'
-  //   })
-  // })
+  intro()
 });
 
 function intro() {
