e0f7a992a0ad41c5f3ddb3f3185849e0bd10e597
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-01-30 23:39:24 -0500

    Remove jQuery and use vanilla JS

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -1,49 +1,56 @@
-function highlight(letter) {
-  var lines = [];
-  $('line').css('stroke', 'transparent');
-  switch(letter) {
+const highlight = (letter) => {
+  let lineSelectors;
+  document.querySelectorAll('line').forEach((line) => {
+    line.style.stroke = 'transparent';
+  });
+  switch (letter.toUpperCase()) {
     case 'S':
-      lines = [$('.top-line'), $('.diag-line'), $('.btm-line')];
+      lineSelectors = ['.top-line', '.diag-line', '.btm-line'];
       break;
     case 'E':
-      lines = [$('.top-line'), $('.left-line'), $('.mid-line'), $('.btm-line')];
+      lineSelectors = ['.top-line', '.left-line', '.mid-line', '.btm-line'];
       break;
     case 'A':
-      lines = [$('.left-line'), $('.mid-line'), $('.diag-line')];
+      lineSelectors = ['.left-line', '.mid-line', '.diag-line'];
       break;
     case 'N':
-      lines = [$('.left-line'), $('.diag-line'), $('.right-line')];
+      lineSelectors = ['.left-line', '.diag-line', '.right-line'];
       break;
+    default:
+      lineSelectors = [];
   }
-  for (var i = 0; i < lines.length; i++) {
-    lines[i].css('stroke', '#000');
-  }
-}
+  lineSelectors.forEach((line) => {
+    document.querySelector(line).style.stroke = '#000';
+  });
+};
 
-function intro() {
+const intro = () => {
+  const overlay = document.querySelector('.overlay');
+
   if (screen.width < 770) {
-    return $('.overlay').hide();
+    overlay.style.display = 'none';
+    return;
   }
-  return setTimeout(function () {
+  setTimeout(() => {
     highlight('S');
-    setTimeout(function () {
+    setTimeout(() => {
       highlight('E');
-      setTimeout(function () {
+      setTimeout(() => {
         highlight('A');
-        setTimeout(function () {
+        setTimeout(() => {
           highlight('N');
-          setTimeout(function () {
-            $('line').css('stroke', '#000');
-            setTimeout(function () {
-              $('.overlay').fadeOut(2250);
+          setTimeout(() => {
+            document.querySelectorAll('line').forEach((line) => {
+              line.style.stroke = '#000';
+            });
+            setTimeout(() => {
+              overlay.style.opacity = 0;
             }, 1000);
           }, 1500);
         }, 1000);
       }, 1000);
     }, 1000);
   }, 2000);
-}
+};
 
-$(function () {
-  intro();
-});
+document.addEventListener('DOMContentLoaded', intro);
